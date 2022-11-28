7855db52494b418441cf46879039888429256974
Oskar Zetterberg <oskarzetterberg@gmail.com>
2022-11-28 18:05:30 -0500

    .

--- Flappy.py
+++ Flappy.py
@@ -7,9 +7,9 @@ import itertools
 class Flappy:
 
     def __init__(self, max_x, max_y, step_x, step_y, epsilon, alpha, gamma):
-        self.state_space_x = self.linspace(0, max_x, step_x)
-        self.state_space_y = self.linspace(-max_y, max_y, step_y)
-        self.qtable = np.zeros((len(self.state_space_x)*len(self.state_space_y), 2))
+        self.state_space_x = self.linspace(0, max_x, step_x) #vector of discritized state space for x
+        self.state_space_y = self.linspace(-max_y, max_y, step_y) #above but for y
+        self.qtable = np.zeros((len(self.state_space_x)*len(self.state_space_y), 2)) #qtable
         self.discrete_state_space = list(itertools.product(self.state_space_x, self.state_space_y))
         self.epsilon = epsilon
         self.alpha = alpha
