04b12e9e082075bb5c264bd1f726eb7be18eeea4
Oskar Zetterberg <oskarzetterberg@gmail.com>
2022-11-28 20:10:48 -0500

    kinda works but the agent is not good

--- Flappy.py
+++ Flappy.py
@@ -6,7 +6,8 @@ import itertools
 
 class Flappy:
 
-    def __init__(self, max_x, max_y, step_x, step_y, epsilon, alpha, gamma):
+    #nbr of decimals of x and y in the rounding needs to be changed if the step_x and step_y are decreased to 0.01
+    def __init__(self, max_x, max_y, step_x, step_y, epsilon, alpha, gamma, nbr_episodes):
         self.state_space_x = self.linspace(0, max_x, step_x) #vector of discritized state space for x
         self.state_space_y = self.linspace(-max_y, max_y, step_y) #above but for y
         self.qtable = np.zeros((len(self.state_space_x)*len(self.state_space_y), 2)) #qtable
@@ -14,10 +15,12 @@ class Flappy:
         self.epsilon = epsilon
         self.alpha = alpha
         self.gamma = gamma
-        self.default_reward = 0
-        self.death_reward = -1
+        self.default_reward = 1
+        self.death_reward = -10
         self.actions = [0, 1]
         self.env = flappy_bird_gym.make("FlappyBird-v0")
+        self.nbr_episodes = nbr_episodes
+        self.scores = np.zeros((nbr_episodes, 1))
 
     #help function
     def linspace(self, start, stop, step_size):
@@ -25,6 +28,8 @@ class Flappy:
 
     #returns the state correspoding to the location (the position index of location in self.discrete_state_space)
     def get_state_from_location(self, location):
+        location = np.around(location, 2)
+        location = (location[0], location[1])
         return self.discrete_state_space.index(location) 
 
     #returns the q_value from the q_table for the pair (location, action)
@@ -46,6 +51,26 @@ class Flappy:
         return self.death_reward if died else self.default_reward
     
     #sarsa learning
-    def sarsa_learning(self):
+    def sample_sarsa_episode(self):
         #resets the environment and returns the initial location (discretized to fit the state-space)
-        location = self.get_discrete_value(self.env.reset())
+        location = self.env.reset()
+        action = self.eps_greedy_action(location)
+        score = 0
+        while True:
+            next_location, reward, done, _ = self.env.step(action)
+            next_action = self.eps_greedy_action(next_location)
+            qvalue = self.get_qvalue(location, action) + self.alpha * (self.get_reward(done) + self.gamma*self.get_qvalue(next_location, next_action) - self.get_qvalue(location, action))
+            self.set_qvalue(location, action, qvalue)
+            location = next_location
+            action = next_action
+            score += reward
+            self.env.render()
+            if done:
+                break
+        return score
+
+    def sarsa_learning(self):
+        for eps_idx in range(self.nbr_episodes):
+            self.scores[eps_idx] = self.sample_sarsa_episode()
+            print((self.scores[eps_idx], eps_idx))
+        return self.scores
--- main.py
+++ main.py
@@ -4,11 +4,8 @@ import numpy as np
 max_x = 2
 max_y = 1
 
-step_x = 1
-step_y = 1
+step_x = 0.01
+step_y = 0.01
 
-bird = Flappy(max_x, max_y, step_x, step_y, epsilon=0.1, alpha=0.5, gamma=0.5)
-bird.set_qvalue((0,-1), 1, 1)
-print(bird.discrete_state_space)
-print(bird.qtable)
-print(bird.eps_greedy_action((0,-1)))
+bird = Flappy(max_x, max_y, step_x, step_y, epsilon=0.1, alpha=0.5, gamma=0.5, nbr_episodes=100)
+print(bird.sarsa_learning())
